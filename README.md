d68c58f716ecd28def2914f256d874fe61bcb8da
unknown <eugene.debrah@hotmail.com>
2020-04-04 13:43:40 +0100

    git push

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Project-Automation-Starter-
+## Inspiration
+This project is inspired by Kalle Halden (https://github.com/KalleHallden). He had the idea to this project and made it working for MacOS. Check his repository out:https://github.com/KalleHallden/ProjectInitializationAutomation
---
+++ Script.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from github import Github
+
+git_username = <UserName>
+git_password = <Password>
+
+path = <Path>
+
+def create_folder_and_repo():
+    folder_name = str(sys,argv[1])
+    public_private = str(sys.argv[2])
+    os.makedirs(path+folder_name)
+
+    user = Github(git_username, git_password).get_user()
+     
+     if (public_private == "private")
+         print('PRIVATE')
+         user.create_repo(folder_name, private=True)
+    else:
+        print('PUBLIC')
+        user.create_repo(folder_name)
+
+    print("You Have Created A Repository {}".format(folder_name))
+
+if __name__ == "__main__"
+    create_folder_and_repo()
